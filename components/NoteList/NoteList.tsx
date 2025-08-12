801320807d9014c3f4ac6abba45d91ee2ad6452d
Daria_H <dholubenna@gmail.com>
2025-08-12 20:05:14 +0300

    fix type num on str

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -13,7 +13,7 @@ export default function NoteList({ notes }: NoteListProps) {
   const queryClient = useQueryClient();
 
   const deleteMutation = useMutation({
-    mutationFn: (id: number) => deleteNote(id),
+    mutationFn: (id: string) => deleteNote(id),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["notes"] });
       toast.success("Note successfully deleted!");
